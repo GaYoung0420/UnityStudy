e2fc6c9ffc6a3711ceb4226e175147c9cc1c4062
GaYoung0420 <ciks2508@naver.com>
2022-01-10 22:06:41 +0900

    2022-01-10

---
+++ UnityStudy_CS/032_Check/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * Name: _032_Check
+ * DESC: GetType()함수로(int,string,float,char)형 CTS표준 출력하기
+-----------------------------------------------------------------------------*/
+
+namespace _032_Check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            string b = "AAA";
+            float c = 0.123f;
+            char d = 'A';
+            Console.WriteLine("a: {0}. a.GetType(): {1}", a, a.GetType());
+            Console.WriteLine("b: {0}. b.GetType(): {1}", b, b.GetType());
+            Console.WriteLine("c: {0}. c.GetType(): {1}", c, c.GetType());
+            Console.WriteLine("d: {0}. a.GetType(): {1}", d, d.GetType());
+        }
+        /*
+            a: 100. a.GetType(): System.Int32
+            b: AAA. b.GetType(): System.String
+            c: 0.123. c.GetType(): System.Single
+            d: A. a.GetType(): System.Char
+         */
+    }
+}
---
+++ UnityStudy_CS/033_Check/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * Name: _033_Check
+ * DESC: 간단한 성적 프로그램 만들기
+-----------------------------------------------------------------------------*/
+
+namespace _033_Check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string temp;
+            int ko, eng, math, sci, sum;
+            float avg;
+            Console.Write("국어 점수 입력하세요?");
+            temp = Console.ReadLine();
+            ko = int.Parse(temp);
+            Console.Write("영어 점수 입력하세요?");
+            temp = Console.ReadLine();
+            eng = int.Parse(temp);
+            Console.Write("수학 점수 입력하세요?");
+            temp = Console.ReadLine();
+            math = int.Parse(temp);
+            Console.Write("과학 점수 입력하세요?");
+            temp = Console.ReadLine();
+            sci = int.Parse(temp);
+            sum = ko + eng + math + sci;
+            avg = sum / 4.0f;
+            Console.WriteLine("국어: {0}  영어: {1} 수학:{2}  과학: {3}", ko, eng, math, sci
+                );
+            Console.WriteLine("총점: {0}  평균: {1}",sum,avg);
+            /*
+                국어 점수 입력하세요?75
+                영어 점수 입력하세요?80
+                수학 점수 입력하세요?45
+                과학 점수 입력하세요?90
+                국어: 75  영어: 80 수학:45  과학: 90
+                총점: 290  평균: 72.5
+             */
+        }
+    }
+}
