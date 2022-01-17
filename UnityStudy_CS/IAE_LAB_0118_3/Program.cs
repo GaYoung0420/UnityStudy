402fc8e5eb4577bff6892032ed3d8dfe051898ea
GaYoung0420 <ciks2508@naver.com>
2022-01-18 01:13:57 +0900

    0118-IAE_LAB_3

--- UnityStudy_CS/IAE_LAB_0118_3/Program.cs
+++ UnityStudy_CS/IAE_LAB_0118_3/Program.cs
@@ -9,20 +9,33 @@ namespace IAE_LAB_0118_3
    
     internal class Program
     {
-         public int[] solution(int[] arr, int[,] commands)
+         static int[] solution(int[] arr, int[,] commands)
         {
-            int[] result = new int[] { };
+            int[] result = new int[commands.GetLength(0)];
 
+            for(int i = 0; i < commands.GetLength(0); i++)
+            {
+                int first = commands[i, 0] -1;
+                int last = commands[i, 1] -1;
+                int index = commands[i, 2] -1;
+                int[] temp = new int[(last - first)+1];
+                // Array.Copy(a, 1, b, 0, 3);
+                // a = 소스 배열, 1 = 소스 배열에서 인덱스 시작, b = 대상 배열, 0 = 대상 배열의 시작 색인, 3 = 복사 할 요소
+                Array.Copy(arr, first, temp, 0, last - first + 1);
+                Array.Sort(temp);
+                printArray(temp);
+                result[i] = temp[index];
+            }
 
             return result;
         }
         static void printArray(int[] arr)
         {
-            Console.Write("array: [");
+            Console.Write("[");
             for (int i = 0; i < arr.Length; i++)
             {
                 if (i == arr.Length - 1)
-                    Console.WriteLine(arr[i] + "]");
+                    Console.Write(arr[i] + "]");
                 else
                     Console.Write(arr[i] + ",");
             }
@@ -31,7 +44,17 @@ namespace IAE_LAB_0118_3
         {
             Console.WriteLine("******** 3) K번째수 ********");
             int[] arr = { 1, 5, 2, 6, 3, 7, 4 };
+            int[,] commands = { { 2, 5, 3}, { 4, 4, 1}, { 1, 7, 3} };
+            Console.Write("array: ");
             printArray(arr);
+            Console.WriteLine();
+            Console.Write("commands: ");
+            int[] result = solution(arr, commands);
+            Console.WriteLine();
+            Console.Write("result array: ");
+            printArray(result);
+            Console.WriteLine();
+
         }
     }
 }
