107c445ad2448f0b79ca1ef5c1cb67c8ec720f80
GaYoung0420 <ciks2508@naver.com>
2022-01-17 23:13:14 +0900

    2022-01-17-notebook

---
+++ UnityStudy_CS/040_Operator_Bit/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _040_Operator_Bit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            int b = 22;
+            
+            int c = a & b;
+            Console.WriteLine("a & b: " + c);
+
+            int d = a | b;
+            Console.WriteLine("a | b: " + d);
+
+            int e = a ^ b;
+            Console.WriteLine("a ^ b: " + e);
+
+            int f = a << 2;
+            Console.WriteLine("a << 2: " + f);
+            Console.WriteLine("a << 1: " + (a << 1)); //왼쪽으로 1시프트 2배 증가(곱셈연산)
+
+            int g = 20 >> 2;
+            Console.WriteLine("20 >. 2: " + g);
+            Console.WriteLine("20 >> 1: " + (20 >> 1)); //오른쪽으로 1시프트하면 2로 나눔(나눗셈 연산)
+
+            int h = ~b;
+            Console.WriteLine("h = ~b: " + h);
+
+            int i = (~b) >> 2;
+            Console.WriteLine("i = (~b) >> 2: " + i);
+
+            string s = Convert.ToString(a,2).PadLeft(32,'0');
+            Console.WriteLine("s: " + s);
+            s = Convert.ToString(b,2).PadLeft(32,'0');
+
+        }
+        /*
+            a & b: 6
+            a | b: 31
+            a ^ b: 25
+            a << 2: 60
+            a << 1: 30
+            20 >. 2: 5
+            20 >> 1: 10
+            h = ~b: -23
+            i = (~b) >> 2: -6
+            s: 00000000000000000000000000001111
+         
+         
+         */
+    }
+}
---
+++ UnityStudy_CS/CheckPoint03/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CheckPoint03
+{
+    internal class Program
+    {
+        const int DELAY_TIME = 300;
+        static void UpdateView(char[] tile, int[,] map)
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(tile[map[i, j]]);
+                    if (j == map.GetLength(1) - 1)
+                        Console.WriteLine();
+                }
+            }
+        }
+
+        static void ClearView()
+        {
+            Thread.Sleep(DELAY_TIME);
+            Console.Clear();
+        }
+
+        static void UpdateGo(int[] arrIndexX, int[,] map)
+        {
+            for (int i = 0; i < arrIndexX.Length; i++)
+            {
+                int indexMaxX = i + 1;
+                int indexY = arrIndexX[i];
+
+                int temp = map[i + 1, indexY];
+                map[indexMaxX, indexY + 1] = temp;
+                map[indexMaxX, indexY] = 0;
+
+                arrIndexX[i]++;
+            }
+        }
+
+        static bool UpdateRandomGo(int[] arrIndexX, int[,] map, Random rand)
+        {
+            bool isFinish = false;
+            for (int i = 0; i < arrIndexX.Length; i++)
+            {
+                if (arrIndexX[i] >= 19)
+                {
+                    isFinish = true;
+                    break;
+                }
+
+            }
+
+            int rndIndex = rand.Next(0, 5);
+            int indexY = arrIndexX[rndIndex];
+
+            int temp = map[rndIndex + 1, indexY];
+            map[rndIndex + 1, indexY + 1] = temp;
+            map[rndIndex + 1, indexY] = 0;
+
+            arrIndexX[rndIndex]++;
+
+            return isFinish;
+        }
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            char[] tile = {' ','-','|','1','2','3','4','5' };
+            
+            int[,] map = new int[7,22]
+            {
+               //0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0},
+                {4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0},
+                {5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0},
+                {6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0},
+                {7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+            };
+
+            int[] arrIndexX = { 0, 0, 0, 0, 0 };
+            bool isFinish = false;
+
+            while (true)
+            {
+                
+
+                UpdateGo(arrIndexX,map);
+                isFinish = UpdateRandomGo(arrIndexX, map,rand);
+
+                UpdateView(tile, map);
+                if (isFinish)
+                {
+                    Console.WriteLine();
+                    for (int i = 0; i < arrIndexX.Length; i++)
+                    {
+                        if (arrIndexX[i] >= 19)
+                        {
+                            Console.Write("달리기 결과: {0}번 1등",(i + 1));
+                            break;
+                        }
+
+                    }
+                    Console.Write("\n다시 시작하려면 0을 입력하세요");
+                    string inputStr = Console.ReadLine();
+                    if(inputStr == "0")
+                    {
+                        
+                        for(int i = 0; i < arrIndexX.Length; i++)
+                        {
+                            map[i + 1, arrIndexX[i]] = 0;
+                            arrIndexX[i] = 0;
+                            map[i + 1, 20] = 2;
+                            map[i + 1, 0] = 3 + i;
+                        }
+                        
+                    }
+                    else
+                    {
+                        Console.Write("\n나가기");
+                        break;
+                    }
+                }
+                ClearView();
+            }
+            
+        }
+    }
+}
